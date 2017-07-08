b22744e535c826849e8e00ffcb88c73ab0c84103
yan dong <yandong_0475@163.com>
2017-07-08 17:40:13 +0800

    shipping

--- src/main/java/com/yandong/dao/ShippingMapper.java
+++ src/main/java/com/yandong/dao/ShippingMapper.java
@@ -2,6 +2,9 @@ package com.yandong.dao;
 
 import com.yandong.pojo.Shipping;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ShippingMapper {
     int deleteByPrimaryKey(Integer id);
@@ -15,4 +18,13 @@ public interface ShippingMapper {
     int updateByPrimaryKeySelective(Shipping record);
 
     int updateByPrimaryKey(Shipping record);
+
+    int deleteByShippingIdUserId(@Param("userId")Integer userId, @Param("shippingId") Integer shippingId);
+
+    int updateByShipping(Shipping record);
+
+    Shipping selectByShippingIdUserId(@Param("userId")Integer userId,@Param("shippingId") Integer shippingId);
+
+    List<Shipping> selectByUserId(@Param("userId")Integer userId);
+
 }
